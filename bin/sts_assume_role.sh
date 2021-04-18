7e832d5a508f3af3b6cfe35601680620b7935af4
pooyavahidi <56851682+pooyavahidi@users.noreply.github.com>
2021-04-18 17:23:27 +1000

    Simplify the aws sts assume role functions

--- bin/sts_assume_role.sh
+++ bin/sts_assume_role.sh
@@ -3,7 +3,8 @@
 # Input variables
 mfa_serial_number=$1 #"arn:aws:iam::trusted_account_id:mfa/myuser"
 role_arn=$2 #"arn:aws:iam::trusting_account_id:role/role_name"
-session_name=$3 #"assumerole_my_session"
+
+set -u
 
 # Reset the environment variables
 export AWS_ACCESS_KEY_ID=
@@ -20,23 +21,30 @@ printf "Enter the session duration in hours [default is 1]: "; read duration
 duration_seconds=$(($duration*3600))
 
 # Get the code and sts token
-printf "Enter the MFA code: "; read code
-res="$(aws sts assume-role --role-arn $role_arn --role-session-name $session_name --serial-number $mfa_serial_number --token-code $code --duration-seconds $duration_seconds)"
+printf "Enter the MFA code: "; read token_code
 
-# Parse the json result using python
-$(python3 -c "import sys, json; data = json.loads(''.join(sys.argv[1:]));print (f'export AWS_ACCESS_KEY_ID={data[\"Credentials\"][\"AccessKeyId\"]} export AWS_SECRET_ACCESS_KEY={data[\"Credentials\"][\"SecretAccessKey\"]} export AWS_SESSION_TOKEN={data[\"Credentials\"][\"SessionToken\"]}')" $res 2>&1)
+# Get the credential
+credentials=$(aws sts assume-role \
+--role-arn ${role_arn} \
+--role-session-name $(date '+%Y%m%d%H%M%S%3N') \
+--duration-seconds ${duration_seconds} \
+--serial-number ${mfa_serial_number} \
+--query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' \
+--output text \
+--token-code ${token_code} \
+)
 
-# Set the account id to the id of trusting account
+export AWS_ACCESS_KEY_ID=$(echo $credentials | cut -d' ' -f1)
+export AWS_SECRET_ACCESS_KEY=$(echo $credentials | cut -d' ' -f2)
+export AWS_SESSION_TOKEN=$(echo $credentials| cut -d' ' -f3)
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
-
-# Unset the temp variables
-unset mfa_serial_number
-unset role_arn
-unset session_name
-unset code
-unset duration
-unset duration_seconds
-unset res
 
 # get caller identity after the assume role
 aws sts get-caller-identity
+
+unset credentials
+unset mfa_serial_number
+unset role_arn
+unset token_code
+unset duration
+unset duration_seconds
