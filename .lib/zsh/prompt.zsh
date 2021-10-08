30c0be680967a77f1a0ce4035bcf0d73d029376e
pooyavahidi <56851682+pooyavahidi@users.noreply.github.com>
2021-10-09 07:54:35 +1100

    Update zsh prompt to support ssh

--- .lib/zsh/prompt.zsh
+++ .lib/zsh/prompt.zsh
@@ -6,6 +6,16 @@ setopt PROMPT_SUBST
 # To set the prompt to the basic mode use the following.
 # PROMPT="%B%F{green}%n@%m%f %1~ %b%# "
 
-# Set the zsh prompt 
-PROMPT="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
-PROMPT+=" %{$fg[cyan]%}%c%{$reset_color%} "
+# Set the arrow. green if successful, red if error
+PROMPT="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ ) "
+
+# If connected via SSH, show the user@hostname
+if [[ "${SSH_TTY}" ]] || [[ "${SSH_CONNECTION}" ]]; then
+    PROMPT+="%{$fg[green]%}%n@%m "
+fi;
+
+# Show directory info
+PROMPT+="%{$fg[cyan]%}%c"
+
+# Reset the color for the user input
+PROMPT+="%{$reset_color%} "
