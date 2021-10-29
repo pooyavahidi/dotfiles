5a545e39e8d08340992da138f6b71d3e13f8a62d
pooyavahidi <56851682+pooyavahidi@users.noreply.github.com>
2021-10-29 12:23:59 +1100

    Update docker.sh

--- .lib/docker.sh
+++ .lib/docker.sh
@@ -11,35 +11,55 @@ export DOCKER_BUILDKIT=1
 #######################################
 # aliases
 #######################################
-alias d='docker'
+alias d="docker"
+alias dcl="docker container ls -a"
+alias dil="docker image ls -a"
+alias dcrm="docker container rm"
+alias dirm="docker image rm"
+alias dvl="docker volume ls"
+
+# Get container IP
+alias dcip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
+
+# Prune images
+alias diprune="docker image prune -a -f"
+
+# Prune containers
+alias dcprune="docker container prune -f"
+
+# Remove unused data
+alias dsysprune="docker system prune -a --volumes"
 
 
 #######################################
 # functions
 #######################################
 
-function docker-prune-all(){
-    docker container prune -f
-    docker image prune -f -a
+# Stop all containers
+function dcstop () {
+    docker container stop $(docker container ls -a -q)
 }
 
-function docker-remove-containers {
-    # Stop all the containers
-    docker container ls | awk 'NR>1 {print $1}' | xargs docker container stop
-
-    # Remove all the stopped containers
-    docker container prune -f
-}
-
-function docker-remove-all {
-    # Stop and remove all the containers
-    docker-remove-containers
-
-    # Remove all the images 
-    docker image prune -f -a
-}
-
-function docker-restart {
+# Restart docker service
+function drestart () {
     sudo systemctl restart docker
+}
+
+function dcstats() {
+    if [[ $# -eq 0 ]]; then
+        docker container stats --no-stream;
+    else
+        docker container stats --no-stream | grep $1;
+    fi
+}
+
+# If path '/' is mounted to `overlay`, it's most likely inside a container
+function _is_in_container() {
+    if findmnt > /dev/null 2>&1 && \
+        [[ $(findmnt / -o SOURCE | awk 'NR>1 {print $1}') == "overlay" ]]; then
+            return 0
+    else
+        return 1
+    fi
 }
 
