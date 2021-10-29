539f2ae6c6f603818689a01b307caf02ff138e33
pooyavahidi <56851682+pooyavahidi@users.noreply.github.com>
2021-10-29 12:22:51 +1100

    Update tmux.sh

--- .lib/tmux.sh
+++ .lib/tmux.sh
@@ -5,18 +5,22 @@
 # aliases
 #######################################
 alias t='tmux -u'
-
 # tmux list of sessions
 alias tl='tmux ls'
-
-# tmux attach to the last session
-alias ta='tmux -u attach'
-
-# tmux attach to session or create a new one
-alias tat='tmux -u attach -t $1 > /dev/null 2>&1 || tmux -u new-session -s $1'
+alias tkill='tmux kill-session -t'
 
 
 #######################################
 # functions
 #######################################
+
+function ta() {
+    if [[ $# -eq 0 ]]; then
+        # Attach to the last session
+        tmux -u attach
+    else
+        # Try to attach to the given session, if doesn't exist, create a new one
+        tmux -u attach -t $1 > /dev/null 2>&1 || tmux -u new-session -s $1
+    fi
+}
 
