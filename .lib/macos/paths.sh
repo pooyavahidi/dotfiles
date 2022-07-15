76e068311a2f848365fc058ec376f9f36a8abe83
pooyavahidi <56851682+pooyavahidi@users.noreply.github.com>
2022-07-15 16:50:36 +1000

    feat: add python bin directory to the macos paths.sh

--- .lib/macos/paths.sh
+++ .lib/macos/paths.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# Add python bin directory
+export PATH="/Users/$USER/Library/Python/3.8/bin:$PATH"
+
 # Add Visual Studio Code (code) to the PATH
 export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH";
