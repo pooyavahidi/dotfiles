5cc8609c2b096a7d235549097afc011ce7c2ec89
pooyavahidi <56851682+pooyavahidi@users.noreply.github.com>
2022-11-28 11:58:36 +1100

    fix: update python version to 3.9 in macos env_variables.sh

--- .lib/macos/env_variables.sh
+++ .lib/macos/env_variables.sh
@@ -5,7 +5,7 @@ export VSCODE_USER_HOME="$HOME/Library/Application Support/Code/User"
 
 
 # Add python bin directory
-export PATH="/Users/$USER/Library/Python/3.8/bin:$PATH"
+export PATH="/Users/$USER/Library/Python/3.9/bin:$PATH"
 
 # Add Visual Studio Code (code) to the PATH
 export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH";
