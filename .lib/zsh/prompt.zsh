4879412fbd1af4640f9838205819970014b90a75
pooyavahidi <56851682+pooyavahidi@users.noreply.github.com>
2023-03-10 14:43:41 +1100

    refactor: zsh/prompt.sh clean up.

--- .lib/zsh/prompt.zsh
+++ .lib/zsh/prompt.zsh
@@ -3,30 +3,33 @@
 # Enable parameter expansion, command substitution in prompts
 setopt PROMPT_SUBST
 
-# Set the prompt variables.
+# Initial prefix and final suffix. Initial prefix shows green if previous
+# command exited without error, otherwise shows red.
+SHELL_PROMPT_INITIAL_PREFIX="%B%(?:%F{green}➜%f:%F{red}➜%f)%b"
+SHELL_PROMPT_FINAL_SUFFIX=" "
+
 # Git Prompt Info
-SHELL_PROMPT_GIT_BRANCH_PREFIX="%B%F{blue}(%f%F{red}"
+SHELL_PROMPT_GIT_BRANCH_PREFIX=" %B%F{blue}(%f%F{red}"
 SHELL_PROMPT_GIT_BRANCH_SUFFIX="%f%F{blue})%f%b "
 SHELL_PROMPT_GIT_DIRTY="%F{yellow}✗%f"
 SHELL_PROMPT_GIT_STATUS_PREFIX=""
-SHELL_PROMPT_GIT_STATUS_SUFFIX=" "
+SHELL_PROMPT_GIT_STATUS_SUFFIX=""
 SHELL_PROMPT_GIT_AHEAD="%F{yellow}⇡%f"
 SHELL_PROMPT_GIT_BEHIND="%F{yellow}⇣%f"
 
+# user@hostname
 SHELL_PROMPT_USER_HOST="%F{green}%n@%m%f"
-
-# Initial arrow. if the last command was successful, shows green, if error, red.
-SHELL_PROMPT_INITIAL_ARROW="%B%(?:%F{green}➜%f:%F{red}➜%f)%b"
 
 # Current Directory info, to show the full path relative to HOME, replace %c with %~
 SHELL_PROMPT_DIR_INFO="%B%F{cyan}%c%f%b"
 
 
 # Set the PROMPT
-# To set the prompt to the MacOS default use the following.
+#######################################
+# To set the prompt to the MacOS default use the following:
 # PROMPT="%B%F{green}%n@%m%f %1~ %b%# "
 
-PROMPT=$SHELL_PROMPT_INITIAL_ARROW
+PROMPT=$SHELL_PROMPT_INITIAL_PREFIX
 
 # Show user@hostname if connected via SSH or it's inside a container.
 if [[ "${SSH_TTY}" ]] || [[ "${SSH_CONNECTION}" ]] || _is_in_container; then
@@ -35,8 +38,6 @@ fi;
 
 PROMPT+=" $SHELL_PROMPT_DIR_INFO"
 
-# Add git prompt info
-PROMPT+=' $(git_prompt_info)'
+PROMPT+='$(git_prompt_info)'
 
-# Set the default, to make it possible to reset after modifications.
-PROMPT_DEFAULT=$PROMPT
+PROMPT+=$SHELL_PROMPT_FINAL_SUFFIX
