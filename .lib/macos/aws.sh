284a1a727884694e4157b4dee531327be086db3b
pooyavahidi <56851682+pooyavahidi@users.noreply.github.com>
2023-03-13 22:09:00 +1100

    fix: check for errors in macos/aws.sh

--- .lib/macos/aws.sh
+++ .lib/macos/aws.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 # Get aws credentials from a keychain item and set them to the env variables.
-# Id and Key are stored in two lines. Line 1 is Id and line 2 is the Key.
-function aws-set-creds-from-keychain {
+function aws::get-creds-from-keychain {
     local __creds
     local __keychain_item_name
 
-    __keychain_item_name=$1
-    [[ -z $__keychain_item_name ]] \
-        && echo "keychain item name is not provided" && return 1
+    [[ -z "${__keychain_item_name:=$1}" ]] \
+        && echo "Keychain item name is not provided" && return 1
 
-    __creds=$(_keychain_get_password $__keychain_item_name)
+    __creds=$(keychain-get-password $__keychain_item_name)
+    (( $? != 0 )) && return 1
+
+    # Id and Key are stored in two lines. Line 1 is Id and line 2 is the Key.
     export AWS_ACCESS_KEY_ID=$(echo $__creds | awk 'FNR==1')
     export AWS_SECRET_ACCESS_KEY=$(echo $__creds | awk 'FNR==2')
 }
